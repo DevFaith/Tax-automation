b6cafd26dec3c7bc64cc9569b9b19afa717a31a5
DevFaith <waitherafaith@outlook.com>
2025-02-18 14:38:56 +0300

    Refactor Firebase configuration to include additional services and export them

--- tax-automation/src/Firebase.js
+++ tax-automation/src/Firebase.js
@@ -1,21 +1,32 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getFunctions } from "firebase/functions";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCsIVaoe8PlEYrSjCih1QRip3XDrWj4u_A",
+  authDomain: "tax-automation-ce2b2.firebaseapp.com",
+  projectId: "tax-automation-ce2b2",
+  storageBucket: "tax-automation-ce2b2.firebasestorage.app",
+  messagingSenderId: "539272022011",
+  appId: "1:539272022011:web:550035276bbf2b4636ed5e"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
+const database = getDatabase(app);
+const functions = getFunctions(app);
 
-export default app;
+export { db, storage, auth, database, functions }
+
+export default app
